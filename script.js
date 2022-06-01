c71c8af594f2169f4ccef55e688109bd3d89580d
Abanoob Abdelmasih <abanoob.naji@hotmail.com>
2022-06-02 02:28:13 +0300

    added the score system and final winner popup

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 "use strict";
 
 const btns = document.querySelectorAll(".btn");
+const gameStatus = document.querySelector(".game-status");
+
+const backgroundPopup = document.querySelector(".background-popup");
+const popupText = document.querySelector(".popup-text");
+
+const playerScoreDisplay = document.querySelector(".player-score-span");
+const computerScoreDisplay = document.querySelector(".computer-score-span");
 
 let playerScore = 0,
   computerScore = 0,
@@ -25,7 +32,10 @@ function playRound(playerSelection, computerSelection) {
     (computerSelection === "rock" && playerSelection === "paper") ||
     (computerSelection === "paper" && playerSelection === "scissors")
   ) {
-    ++playerScore;
+    playerScoreDisplay.textContent = ++playerScore;
+    if (playerScore === 5) {
+      gameEnds(playerSelection);
+    }
     return `${playerSelection} beats ${computerSelection} player win`;
   }
 
@@ -34,8 +44,22 @@ function playRound(playerSelection, computerSelection) {
     (computerSelection === "paper" && playerSelection === "rock") ||
     (computerSelection === "scissors" && playerSelection === "paper")
   ) {
-    ++computerScore;
+    computerScoreDisplay.textContent = ++computerScore;
+    if (computerScore === 5) {
+      gameEnds(computerSelection);
+    }
     return `${computerSelection} beats ${playerSelection} player lose`;
+  }
+}
+
+// ///////////////////////////////////////////////////////////////////////////////////
+
+function gameEnds(player) {
+  backgroundPopup.classList.remove("hidden");
+  if (player === playerSelection) {
+    popupText.textContent = "Player Won";
+  } else {
+    popupText.textContent = "Computer Won";
   }
 }
 
@@ -43,14 +67,10 @@ function playRound(playerSelection, computerSelection) {
 
 btns.forEach((btn) => {
   btn.addEventListener("click", (e) => {
-    // console.log(e.target.id);
     let playerSelection = e.target.id;
     let computerSelection = computerPlay();
-    console.log(playRound(playerSelection, computerSelection));
+    gameStatus.textContent = playRound(playerSelection, computerSelection);
   });
 });
 
-// function game() {
-// }
 
-// game();
